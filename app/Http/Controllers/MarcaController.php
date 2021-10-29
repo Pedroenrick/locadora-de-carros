41b68f9e090dab551735e69999d782059607ad48
Pedroenrick <pedro_penna22@hotmail.com>
2021-10-29 18:46:47 -0300

    adicionando controllers de Marca

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+use function PHPUnit\Framework\isEmpty;
+
+class MarcaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $marcas = Marca::all();
+            return $marcas;
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $marca = Marca::create($request->all());
+            return $marca;
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marca $marca)
+    {
+        try {
+            $marca = Marca::find($marca->id);
+            return $marca;
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Marca $marca)
+    {
+        try {
+            $marca = Marca::find($marca->id);
+            $marca->update($request->all());
+            return $marca;
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Marca $marca)
+    {
+        try {
+            $marca = Marca::find($marca->id);
+            $marca->delete();
+            return $marca;
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
