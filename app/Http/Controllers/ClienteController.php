1aef39921df898a69513ee634c2c4153f26cf07c
Pedroenrick <pedro_penna22@hotmail.com>
2021-11-07 12:45:41 -0300

    adicionando CRUD locação

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -4,19 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\Cliente;
 use Illuminate\Http\Request;
+use App\Repositories\ClienteRepository;
 
 class ClienteController extends Controller
 {
+
+
+    public function __construct(Cliente $cliente)
+    {
+        $this->cliente = $cliente;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+
         try {
-            $clientes = Cliente::all();
-            return response()->json($clientes, 200);
+            $clienteRepository = new ClienteRepository($this->cliente);
+
+            if ($request->has('filtro')) {
+                $clienteRepository->filtro($request->filtro);
+            }
+
+            if ($request->has('atributos')) {
+                $clienteRepository->selectAtributos($request->atributos);
+            }
+
+            return response()->json($clienteRepository->getResultado(), 200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
@@ -30,7 +47,15 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->cliente->rules());
+        try {
+            $cliente = $this->cliente->create([
+                'nome' => $request->nome,
+            ]);
+            return response()->json($cliente, 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -39,9 +64,18 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function show(Cliente $cliente)
+    public function show($id)
     {
-        //
+        try {
+            $cliente = $this->cliente->find($id);
+
+            if (!$cliente) {
+                return response()->json(['error' => 'cliente não encontrado'], 404);
+            }
+            return response()->json($cliente, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
 
@@ -52,9 +86,37 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Cliente $cliente)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $cliente = $this->cliente->find($id);
+
+            if (!$cliente) {
+                return response()->json(['error' => 'Impossivel realizar atualização. O recurso solicitado não existe'], 404);
+            }
+
+            if ($request->method() === 'PATCH') {
+                $dynamicRules = array();
+                //percorrendo array de regras definidas no model
+
+                foreach ($cliente->rules() as $input => $rule) {
+                    if (array_key_exists($input, $request->all())) {
+                        $dynamicRules[$input] = $rule;
+                    }
+                }
+
+                $request->validate($dynamicRules);
+            } else {
+                $request->validate($cliente->rules());
+            }
+
+            $cliente->fill($request->all());
+            $cliente->save();
+
+            return response()->json($cliente, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -63,8 +125,19 @@ class ClienteController extends Controller
      * @param  \App\Models\Cliente  $cliente
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Cliente $cliente)
+    public function destroy($id)
     {
-        //
+        try {
+            $cliente = $this->cliente->find($id);
+            if (!$cliente) {
+                return response()->json(['error' => 'Impossivel realizar exclusão. O recurso solicitado não existe'], 404);
+            }
+
+            $cliente->delete();
+
+            return response()->json(['msg' => 'cliente removido com sucesso'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
--- app/Http/Controllers/LocacaoController.php
+++ app/Http/Controllers/LocacaoController.php
@@ -4,27 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Locacao;
 use Illuminate\Http\Request;
+use App\Repositories\LocacaoRepository;
 
 class LocacaoController extends Controller
 {
+
+    public function __construct(Locacao $locacao)
+    {
+        $this->locacao = $locacao;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        try {
+            $locacaoRepository = new LocacaoRepository($this->locacao);
+
+            if ($request->has('filtro')) {
+                $locacaoRepository->filtro($request->filtro);
+            }
+
+            if ($request->has('atributos')) {
+                $locacaoRepository->selectAtributos($request->atributos);
+            }
+
+            return response()->json($locacaoRepository->getResultado(), 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -35,7 +46,22 @@ class LocacaoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->locacao->rules());
+        try {
+            $locacao = $this->locacao->create([
+                'cliente_id'=> $request->cliente_id,
+                'carro_id'=> $request->carro_id,
+                'data_inicio_periodo'=> $request->data_inicio_periodo,
+                'data_final_previsto_periodo'=> $request->data_final_previsto_periodo,
+                'data_final_realizado_periodo'=> $request->data_final_realizado_periodo,
+                'valor_diaria'=> $request->valor_diaria,
+                'km_inicial'=> $request->km_inicial,
+                'km_final' => $request->km_final,
+            ]);
+            return response()->json($locacao, 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -44,20 +70,18 @@ class LocacaoController extends Controller
      * @param  \App\Models\Locacao  $locacao
      * @return \Illuminate\Http\Response
      */
-    public function show(Locacao $locacao)
+    public function show($id)
     {
-        //
-    }
+        try {
+            $locacao = $this->locacao->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Locacao $locacao)
-    {
-        //
+            if (!$locacao) {
+                return response()->json(['error' => 'locacao não encontrado'], 404);
+            }
+            return response()->json($locacao, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -67,9 +91,37 @@ class LocacaoController extends Controller
      * @param  \App\Models\Locacao  $locacao
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Locacao $locacao)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $locacao = $this->locacao->find($id);
+
+            if (!$locacao) {
+                return response()->json(['error' => 'Impossivel realizar atualização. O recurso solicitado não existe'], 404);
+            }
+
+            if ($request->method() === 'PATCH') {
+                $dynamicRules = array();
+                //percorrendo array de regras definidas no model
+
+                foreach ($locacao->rules() as $input => $rule) {
+                    if (array_key_exists($input, $request->all())) {
+                        $dynamicRules[$input] = $rule;
+                    }
+                }
+
+                $request->validate($dynamicRules);
+            } else {
+                $request->validate($locacao->rules());
+            }
+
+            $locacao->fill($request->all());
+            $locacao->save();
+
+            return response()->json($locacao, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -78,8 +130,19 @@ class LocacaoController extends Controller
      * @param  \App\Models\Locacao  $locacao
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Locacao $locacao)
+    public function destroy($id)
     {
-        //
+        try {
+            $locacao = $this->locacao->find($id);
+            if (!$locacao) {
+                return response()->json(['error' => 'Impossivel realizar exclusão. O recurso solicitado não existe'], 404);
+            }
+
+            $locacao->delete();
+
+            return response()->json(['msg' => 'locacao removida com sucesso'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
